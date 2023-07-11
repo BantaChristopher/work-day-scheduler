bcc0c7ed3aec0aacbeb7df6edfacce3fa7323f3f
Christopher Banta <chris.banta@rocketmail.com>
2023-07-10 23:46:35 -0500

    Finished app, added all JS functionality

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,22 @@ $(function () {
   $(".saveBtn").on("click", function() {
       var hour = $(this).parent().attr('id');
       var input = $(this).parent().children('textarea').val();
-
+      
       localStorage.setItem(hour, input);
+      $('#saveState').text("Save Successful!✅");
+      var time = 2;
+      var remove = 0;
+      setInterval(function() {
+        time--;
+        if(time == remove){
+          $('#saveState').text(" ")
+        }
+      }, 1000)
   })
 
   $('.time-block').each( function() {
     var compareTime = $(this).attr('id');
-    // console.log(compareTime)
     var currentTime = dayjs().format('H');
-    // console.log(currentTime)
     if(compareTime < currentTime ) {
       $(this).addClass("past")
     } else if(compareTime > currentTime) {
@@ -20,13 +27,15 @@ $(function () {
     }
   })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  $('.time-block').each( function() {
+    var hour = $(this).attr('id');
+    var temp = localStorage.getItem(hour);
+    console.log(temp);
+    if(temp != null){
+      $(this).children('textarea').val(temp)
+    }
+    })
 
-
-  // TODO: Add code to display the current date in the header of the page.
     var currentDate = dayjs().format('MMM DD, YYYY');
     $('#currentDay').text(currentDate);
 });
